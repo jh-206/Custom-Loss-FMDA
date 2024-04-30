0d3c9e747969e30eb116c1d5a30674c9d5d74aa3
jh-206 <jhirschi206@gmail.com>
2024-04-30 14:08:58 -0600

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,80 +1,79 @@
 import numpy as np
-from functools import singledispatch
-import pandas as pd
-import numbers
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from matplotlib.patches import Polygon
 
-numeric_kinds = {'i', 'u', 'f', 'c'}
+# Map stations, credit 
+    # https://stackoverflow.com/questions/53233228/plot-latitude-longitude-from-csv-in-python-3-6
+    # https://gist.github.com/blaylockbk/79658bdde8c1334ab88d3a67c6e57477
 
-def is_numeric_ndarray(array):
-    if isinstance(array, np.ndarray):
-        return array.dtype.kind in numeric_kinds
-    else:
-        return False
-
-def vprint(*args):
-    import inspect
+def make_st_map(df):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
     
-    frame = inspect.currentframe()
-    if 'verbose' in frame.f_back.f_locals:
-        verbose = frame.f_back.f_locals['verbose']
-    else:
-        verbose = False
+    buf = 4
+    lons = (np.amin(df.lon), np.amax(df.lon))
+    lats = (np.amin(df.lat), np.amax(df.lat))
+    map = Basemap(projection='cyl', llcrnrlat=lats[0]-buf, urcrnrlat=lats[1]+buf,
+                    llcrnrlon=lons[0]-buf,urcrnrlon=lons[1]+buf, resolution="i")
+    map.drawcoastlines()
+    map.drawcountries()
+    map.drawstates()
+
+    map.arcgisimage(service="World_Street_Map", xpixels=1000, verbose=False)
     
-    if verbose: 
-        for s in args[:(len(args)-1)]:
-            print(s, end=' ')
-        print(args[-1])
-        
-        
-## Generic function to hash dictionary of various types
+    x, y = map(df['lon'].values, df['lat'].values)
+    map.scatter(x, y, marker='o')
+    
+    # Add a rectangle representing the bounding box
+    vertices = [map(lons[0], lats[1]), map(lons[1], lats[1]), 
+                map(lons[1], lats[0]), map(lons[0], lats[0])]
+    bounding_box = Polygon(vertices, edgecolor='r', linewidth=1.5, facecolor='none', zorder=5)
 
-@singledispatch
-## Top level hash function with built-in hash function for str, float, int, etc
-def hash2(x):
-    return hash(x)
+    ax.add_patch(bounding_box)
+    
+    return 
 
-@hash2.register(np.ndarray)
-## Hash numpy array, hash array with pandas and return integer sum
-def _(x):
-    # return hash(x.tobytes())
-    return np.sum(pd.util.hash_array(x))
 
-@hash2.register(list)
-## Hash list, convert to tuple
-def _(x):
-    return hash2(tuple(x))
+import plotly.express as px
+import plotly.graph_objects as go
 
-@hash2.register(tuple)
-def _(x):
-    r = 0
-    for i in range(len(x)):
-        r+=hash2(x[i])
-    return r
+# Map stations, credit https://stackoverflow.com/questions/53233228/plot-latitude-longitude-from-csv-in-python-3-6
 
-@hash2.register(dict)
-## Hash dict, loop through keys and hash each element via dispatch. Return hashed integer sum of hashes
-def _(x, keys = None, verbose = False):
-    r = 0 # return value integer
-    if keys is None: # allow user input of keys to hash, otherwise hash them all
-        keys = [*x.keys()]
-    keys.sort()
-    for key in keys:
-        if (verbose): print('Hashing', key)
-        r += hash2(x[key])
-    return hash(r)
+def make_st_map_interactive(df):
+    fig = go.Figure(go.Scattermapbox(
+        lat=df['lat'],
+        lon=df['lon'],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=10,
+            opacity=0.7,
+        ),
+        text=df['stid'],
+        showlegend=False  # Turn off legend
+    ))
 
-def print_args(func, *args, **kwargs):
-# wrapper to trace function call and arguments
-    print(f"Called: {func.__name__}")
-    print("Arguments:")
-    for arg in args:
-        print(f"  {arg}")
-    for key, value in kwargs.items():
-        print(f"  {key}={value}")
-    return func(*args, **kwargs)
-
-def print_args_test():
-    def my_function(a, b):
-        # some code here
-        return a + b
-    print_args(my_function, a=1, b=2)
+    # Add Points
+    center_lon=df['lon'].median()
+    center_lat=df['lat'].median()
+    fig.update_layout(
+        mapbox_style="open-street-map",
+        mapbox_center=dict(lat=center_lat, lon=center_lon)
+    )
+    # Add Lines for Bounding Box
+    
+    fig.add_trace(go.Scattermapbox(
+        mode="lines",
+        lon=[df['lon'].min(), df['lon'].min(), df['lon'].max(), df['lon'].max(), df['lon'].min()],
+        lat=[df['lat'].min(), df['lat'].max(), df['lat'].max(), df['lat'].min(), df['lat'].min()],
+        marker=dict(size=5, color="black"),
+        line=dict(width=1.5, color="black"),
+        showlegend=False
+    ))
+    
+    fig.update_layout(
+        margin={"r":0,"t":0,"l":0,"b":0},
+        mapbox_zoom =5,
+        mapbox_center={"lat": np.median(df.lat), "lon": np.median(df.lon)},  # Center the map on desired location
+    )
+    return fig
