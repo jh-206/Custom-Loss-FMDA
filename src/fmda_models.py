85afe3f9378ecb21f09780999812423336480816
jh-206 <jhirschi206@gmail.com>
2024-05-01 13:06:19 -0600

    Move MLP model to module

--- src/fmda_models.py
+++ src/fmda_models.py
@@ -158,3 +158,81 @@ class LM:
     def get_coefs(self):
         return lm.coef_
 
+# Multilayer Perceptron
+class MLP:
+    """
+    Wrapper class for multilayer perceptron neural network model.
+    Parameters:
+    -----------
+    params : dict
+        Parameters to be passed to the XGBoost model.
+    loss : 
+    Attributes:
+    -----------
+    model : keras.engine.sequential.Sequential
+        Underlying neural network.
+    params : dict
+        Parameters passed to the NN model.
+
+    Methods:
+    --------
+    fit(X_train, y_train):
+        Train the model on the training data.
+    predict(X_test):
+        Make predictions using the trained model.
+    """
+    def __init__(self, params, loss='mean_squared_error'):
+        """
+        Initialize the MLP class.
+        Parameters:
+        -----------
+        params : dict
+            HyperParameters to be passed to the XGBoost model.
+        objective : str or custom func.
+        """
+        self.params = params
+        self.params['loss'] = loss
+        self.model = self._build_model()
+        self.compile_model()
+
+    def _build_model(self):
+        model = tf.keras.Sequential([
+            tf.keras.layers.Dense(self.params['hidden_units'], activation=self.params['activation'], input_shape=(self.params['input_dim'],)),
+            tf.keras.layers.Dropout(self.params['dropout']),  # Dropout layer
+            tf.keras.layers.Dense(1)  # Output layer with a single neuron for regression
+        ])
+        return model
+    def compile_model(self):
+        optimizer=tf.keras.optimizers.Adam(learning_rate=self.params['learning_rate'])
+        self.model.compile(optimizer=optimizer,
+                           loss=self.params['loss'],
+                           metrics=self.params.get('metrics', ['accuracy']))
+    def fit(self, X_train, y_train):
+        """
+        Train the XGBoost model on the training data.
+
+        Parameters:
+        -----------
+        X_train : array-like or sparse matrix of shape (n_samples, n_features)
+            Training input samples.
+        y_train : array-like of shape (n_samples,)
+            Target values.
+        """
+        self.model.fit(X_train, y_train, epochs=params['epochs'], batch_size=params['batch_size'], validation_split=params['validation_split'])
+
+    def predict(self, X_test):
+        """
+        Make predictions using the trained model.
+        Parameters:
+        -----------
+        X_test : array-like or sparse matrix of shape (n_samples, n_features)
+            Test input samples.
+
+        Returns:
+        --------
+        array-like of shape (n_samples,)
+            Predicted target values.
+        """
+        return self.model.predict(X_test)
+    def summary(self):
+        return self.model.summary()
